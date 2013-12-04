f151eae529b34094fe9becc5119c64629365b68a
Richard Heelin <richardh@channelgrabber.com>
2013-12-04 09:19:35 +0000

    CGIV-1013: Switched to using a custom sudoers template

--- recipes/permissions.rb
+++ recipes/permissions.rb
@@ -1,6 +1,9 @@
 sudo 'php5-fpm' do
-    user node['php-fpm']['user']
-    group node['php-fpm']['group']
-    nopasswd true
-    commands ['start php5-fpm', 'stop php5-fpm', 'restart php5-fpm']
+    template 'permissions.erb'
+    variables({
+        :commands => ['start php5-fpm', 'stop php5-fpm', 'restart php5-fpm'],
+        :host => 'ALL',
+        :runas => 'root',
+        :nopasswd => true
+    })
 end
