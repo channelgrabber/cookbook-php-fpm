2b9dee6376bbe07a8f6b3342b59b87069110628d
Richard Heelin <richardh@channelgrabber.com>
2013-12-04 09:36:46 +0000

    CGIV-1013: Can't use node values in template as template gets validated by a method that has no knowledge of the node array

--- recipes/permissions.rb
+++ recipes/permissions.rb
@@ -2,6 +2,8 @@ sudo 'php5-fpm' do
     template 'permissions.erb'
     variables({
         :commands => ['start php5-fpm', 'stop php5-fpm', 'restart php5-fpm'],
+        :user => node['php-fpm']['user'],
+        :group => "%#{node['php-fpm']['group']}",
         :host => 'ALL',
         :runas => 'root',
         :nopasswd => true
