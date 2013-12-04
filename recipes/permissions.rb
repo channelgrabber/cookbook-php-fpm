442b45c50669af27cae57ce64fb377ff16b9f937
Richard Heelin <richardh@channelgrabber.com>
2013-12-04 08:52:41 +0000

    CGIV-1013: Switching to force_default so that it can still be overriden with a override parapeter

--- recipes/permissions.rb
+++ recipes/permissions.rb
@@ -1,4 +1,4 @@
-node.overide['authorization']['sudo']['include_sudoers_d'] = true
+node.force_default['authorization']['sudo']['include_sudoers_d'] = true
 
 sudo 'php5-fpm' do
     user node['php-fpm']['user']
