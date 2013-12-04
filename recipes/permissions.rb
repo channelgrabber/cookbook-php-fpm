c8dbd7cbff6ab10fb09a5f00ff7c65672d593e2d
Richard Heelin <richardh@channelgrabber.com>
2013-12-04 08:56:01 +0000

    CGIV-1013: Ensure that the permissions recipe is run before the sudoers recipe

--- recipes/permissions.rb
+++ recipes/permissions.rb
@@ -1,5 +1,7 @@
 node.force_default['authorization']['sudo']['include_sudoers_d'] = true
 
+include_recipie 'sudo'
+
 sudo 'php5-fpm' do
     user node['php-fpm']['user']
     group node['php-fpm']['group']
