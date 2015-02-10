d6e2055f4b1a1fa5ba6b9aac5da9f25ab6737cf9
Richard Heelin <richardh@channelgrabber.com>
2015-02-10 16:48:15 +0000

    CGIV-4761: As dpkg controls configs we need to teach it to ignore updates to config files

--- recipes/default.rb
+++ recipes/default.rb
@@ -101,7 +101,7 @@ directory "/var/run/php-fpm" do
 end
 
 package php_fpm_service_name do
-  options "--assume-no"
+  options "-o Dpkg::Options::=\"--force-confdef --force-confold\""
   action :upgrade
 end
 
